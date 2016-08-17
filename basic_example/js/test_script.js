c753578af4d1b021daa2e72b241c2c257e531b15
Robmeister20134 <A00226084@student.ait.ie>
2016-08-18 00:27:13 +0100

    Changed implementation to allow the user to specify positioning and changed the layout of the creation of an overlay so that its more uniform, as well as adding default values

--- basic_example/js/test_script.js
+++ basic_example/js/test_script.js
@@ -4,7 +4,19 @@ window.onload = function(){
 		var otherImage = document.getElementById('previewImage2');
 		var canvasFromPage = document.getElementById('imagecanvas');
 		
-		var overlayTest = new OverlayImages(image, otherImage, canvasFromPage);
+		var overlayTest = new OverlayImages({
+			images: {
+				firstImage: image,
+				secondImage: otherImage,
+				canvas: canvasFromPage
+			},
+		},
+		{
+			position: {
+				xAxis: '20',
+				yAxis: '50'
+			}
+		});
 		
 		overlayTest.overlay();
 		
